99b3ea9261ccd8ed6ccd8916d3a8acef9bb8c92c
Rich Wareham <rich@richwareham.com>
2019-06-23 14:53:32 +0100

    add horiz intrerrupt

--- emu/top.v
+++ emu/top.v
@@ -25,12 +25,31 @@ hlinectr hlinectr(
   .reset(reset), .clk(clk), .low_bits(h_low), .term_cnt(h_end)
 );
 
+// horizontal interrupr
+reg h_int_n;
+always @(posedge clk) begin
+  if(reset) begin
+    h_int_n <= 1'b1;
+  end else begin
+    if(h_end) begin
+      h_int_n <= 1'b0;
+    end else if(~h_int_n && cpu_int_ack) begin
+      h_int_n <= 1'b1;
+    end else begin
+      h_int_n <= h_int_n;
+    end
+  end
+end
+
 // cpu
 wire cpu_read_n;
 wire cpu_write_n;
 wire cpu_refresh_n;
 wire cpu_halt_n;
-
+wire cpu_m1_n;
+wire cpu_iorq_n;
+wire cpu_int_n = h_int_n;
+wire cpu_int_ack = ~cpu_m1_n && ~cpu_iorq_n;
 wire [7:0] cpu_d_in = sram_d_out;
 wire [7:0] cpu_d_out = ~cpu_write_n ? cpu_data : 8'hZZ;
 wire [7:0] cpu_data = ~cpu_read_n ? cpu_d_in : 8'hZZ;
@@ -38,9 +57,9 @@ wire [15:0] cpu_addr;
 z80_top_direct_n cpu(
   .nRESET(~cpu_reset), .CLK(cpu_clk),
   .A(cpu_addr), .D(cpu_data),
-  .nBUSRQ(1'b1), .nWAIT(1'b1), .nINT(1'b1), .nNMI(1'b1),
+  .nBUSRQ(1'b1), .nWAIT(1'b1), .nNMI(1'b1), .nINT(cpu_int_n),
   .nRFSH(cpu_refresh_n), .nRD(cpu_read_n), .nWR(cpu_write_n),
-  .nHALT(cpu_halt_n)
+  .nHALT(cpu_halt_n), .nM1(cpu_m1_n), .nIORQ(cpu_iorq_n)
 );
 
 // RAM
